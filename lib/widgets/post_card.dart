ac383ca7317abec958b1471b02c72053e29b8a0b
Василий Худобин <khudobin@vk.com>
2026-03-06 03:35:46 +0300

    Создание карточки поста

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:posts_test/widgets/user_card.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(26),
+        border: Border.all(color: Colors.black12, width: 1),
+        boxShadow: [
+          BoxShadow(offset: Offset(2, 2), color: Colors.black12, blurRadius: 5),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          spacing: 12,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Заголовок поста",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                const Text("#0", style: TextStyle(fontSize: 8)),
+              ],
+            ),
+            const Text(
+              "Текст поста... текст поста... текст поста... текст поста... текст поста... текст поста... ",
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+            ),
+            UserCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
