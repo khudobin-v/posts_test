00a3230314fa712fa403204a71602f548db5eac8
Василий Худобин <khudobin@vk.com>
2026-03-06 03:55:35 +0300

    Применение стилей

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:posts_test/widgets/post_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text("Подключение шрифта")]));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              const SizedBox(height: 48),
+              const Text(
+                "Посты",
+                style: TextStyle(fontSize: 72, fontWeight: FontWeight.w900),
+              ),
+              const Text(
+                "Все посты",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 6),
+              Column(
+                spacing: 6,
+                children: [PostCard(), PostCard(), PostCard()],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
